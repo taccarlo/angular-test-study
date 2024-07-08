af497251d1af4932b32946c7b7adf264dd0d6e50
Carlo Tacchella <carlo.tacchella@acs.it>
2024-07-08 16:04:01 +0200

    new version

--- src/app/sapp.component.ts
+++ src/app/sapp.component.ts
@@ -1,37 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-
-/* TestBed is the main angular utility package */
-/* The describe container contains different blocks (it, beforeEach, xit..) */
-/* the previous block of app.component.spec.ts is app.module.ts */
-
-
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    // componentInstance creates an instance of the class 
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-study' title`, () => {
+  it(`should have as title 'angular-unit-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-study');
+    expect(app.title).toEqual('angular-unit-test');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    /* After creating the component, an instance of the created component is called */
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-study');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-unit-test app is running!');
   });
 });
