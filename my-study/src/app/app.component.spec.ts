61e685ebf0963285d2cebec58be94abda8939ebd
Carlo Tacchella <carlo.tacchella@acs.it>
2024-07-08 15:15:55 +0200

    angular test study, starting

---
+++ my-study/src/app/app.component.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+
+/* TestBed is the main angular utility package */
+/* The describe container contains different blocks (it, beforeEach, xit..) */
+/* the previous block of app.component.spec.ts is app.module.ts */
+
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    // componentInstance creates an instance of the class 
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'my-study' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('my-study');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-study');
+  });
+});
